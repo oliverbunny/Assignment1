c402964b907582be17eeb8bbf0066810414d3def
Oliver Bunny <c3282349@uon.edu.au>
2019-04-04 06:19:42 +0000

    I still dont like programming

--- assignmentMain.c
+++ assignmentMain.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
 
+char message[] = "abcdefghijklmnopqrstuvwxyz";
+
 int main(){
 
+ scanf("%s", message);
+    printf("Message: %s\n", message);
+    
+    int i = 0;
+    while(message[i] != '\0')
+        i++;
+    char encrypted[i] ;
+    
+    int x;
+    for(x=0; x!=\0; x++){
+        if(index ascii is between 97-122)
+        
 int i;
-    char x = 'A';
+char x = 'A';
     scanf("%c", x);
     i = x;
     printf("%c %d", x, i);
