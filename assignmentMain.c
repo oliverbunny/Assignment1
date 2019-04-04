c0169d442d9189f9be930ddee6965d5ab598e30a
Oliver Bunny <c3282349@uon.edu.au>
2019-04-04 08:13:54 +0000

    The stay after workshop because you dont know what youre doing and need to figure it out grind

--- assignmentMain.c
+++ assignmentMain.c
@@ -1,26 +1,46 @@
 #include<stdio.h>
 
-char message[] = "abcdefghijklmnopqrstuvwxyz";
+char message[] = "abcdefghijklmnopqrstuvwxyz";  //The message to be encrypted
 
 int main(){
 
- scanf("%s", message);
+    scanf("%s", message);   
     printf("Message: %s\n", message);
     
-    int i = 0;
-    while(message[i] != '\0')
-        i++;
-    char encrypted[i] ;
+int i = 0;                                  //This block of code counts the
+    while(message[i] != '\0'){                  //length of the "message" array to
+        i++;                                    //give the "encrypted" the same length. 
+char encrypted[i] ;
+    }
+
+int x;
+    for(x=0; message[x] != '\0'; x++){          //For loop is to execute encryption algorithms
+int j;
+    while( j<n ){
+        printf("%c = %d\n", str[j], str[j]);
+        j++;
+    }                                        //for the length of the "message" array only.
+        if(index ascii value is between 97-122)
+    //Convert from lower case char to upper case      
+        else if(index ascii value is between 65-90)
+    //Encrypt char by reducing its ascii value by certain amount
+        else if()   
     
-    int x;
-    for(x=0; x!=\0; x++){
-        if(index ascii is between 97-122)
-        
-int i;
-char x = 'A';
-    scanf("%c", x);
-    i = x;
-    printf("%c %d", x, i);
     
+    
+    
+    
+    
+/* Code for print ASCII values of Index's from string
+  char str[] = "abcdefghijklmnopqrstuvwxyz";
+ 
+    
+    scanf("%s", str);
+
+int j;
+    for(j=0; str[j] != '\0'; j++){
+        printf("%c = %d\n", str[j], str[j]);
+    }
+    */
 return 0;
 }
