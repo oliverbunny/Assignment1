edfb1ae401b727498658d6f1b6599c35c5f1bc93
Oliver Bunny <c3282349@uon.edu.au>
2019-04-10 03:42:31 +0000

    Rotation Encryption done... nearly lol. Modulus doesnt work ugh

--- assignmentMain.c
+++ assignmentMain.c
@@ -1,46 +1,26 @@
-#include<stdio.h>
-
-char message[] = "abcdefghijklmnopqrstuvwxyz";  //The message to be encrypted
-
+#include <stdio.h>
+#include <math.h>
 int main(){
 
-    scanf("%s", message);   
-    printf("Message: %s\n", message);
-    
-int i = 0;                                  //This block of code counts the
-    while(message[i] != '\0'){                  //length of the "message" array to
-        i++;                                    //give the "encrypted" the same length. 
-char encrypted[i] ;
-    }
-
-int x;
-    for(x=0; message[x] != '\0'; x++){          //For loop is to execute encryption algorithms
-int j;
-    while( j<n ){
-        printf("%c = %d\n", str[j], str[j]);
-        j++;
-    }                                        //for the length of the "message" array only.
-        if(index ascii value is between 97-122)
-    //Convert from lower case char to upper case      
-        else if(index ascii value is between 65-90)
-    //Encrypt char by reducing its ascii value by certain amount
-        else if()   
-    
-    
-    
-    
-    
-    
-/* Code for print ASCII values of Index's from string
-  char str[] = "abcdefghijklmnopqrstuvwxyz";
- 
-    
+    int k = 1;  
+    char str[] = "abcDEFghiJklMNopqRstuVwxyz";
     scanf("%s", str);
-
-int j;
+  
+  
+    int j;
     for(j=0; str[j] != '\0'; j++){
-        printf("%c = %d\n", str[j], str[j]);
+        
+        if(str[j]>=97 && str[j]<=122){
+            str[j] = str[j] - 32; 
+        } 
+        
+        if(str[j]>=65 && str[j]<=90){
+            str[j] = (str[j] + k);
+            printf("%c", str[j]);
+        } else 
+        
+        printf("%c", str[j]);
     }
-    */
+
 return 0;
 }
